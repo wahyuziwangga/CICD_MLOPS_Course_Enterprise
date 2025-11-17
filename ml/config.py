286284507f537837cf579c73d8fb42aa93b3a26b
wahyuziwangga <ziwangga3@gmail.com>
2025-11-17 16:07:32 +0700

    Upgrade model: n_estimators 100 -> 200

--- ml/config.py
+++ ml/config.py
@@ -10,7 +10,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 class TrainingConfig:
     random_state: int = 42
     test_size: float = 0.2
-    n_estimators: int = 100 # nanti skenario 2: ganti 50 → 100
+    n_estimators: int = 250 # nanti skenario 2: ganti 50 → 100
     model_dir: Path = BASE_DIR / "models"
     model_path: Path = BASE_DIR / "models" / "model.pkl"
     metrics_path: Path = BASE_DIR / "models" / "metrics.json"
